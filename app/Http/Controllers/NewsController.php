fb4f3a8aff2b6d82d05145488567f2597fd73ad8
addam01 <addam01@hotmail.com>
2015-10-16 12:27:00 +0800

    Update Controller
    Added New Controller
    Added new Routing

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+	/**
+	* 
+	*/
+	class NewsController extends Controller
+	{
+		
+		public function index()
+		{
+			return "Reserved for news page";
+		}
+	}
+?>
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,41 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/hello',function(){
+	return "Hello World";
+});
+Route::get('/hello/{name?}',function($name=null){
+	return 'Hello' . $name;
+});
+
+Route::get('/student/{i}',function($i){
+	$students = ['Addam','Kat','Derp'];
+	return $students[$i];
+});
+Route::get('/students/json',function(){
+	$students = ['Addam','Kat','Derp'];
+	return response()->json($students);
+});
+
+// 2-D JSON
+Route::get('/students/2Djson',function(){
+	$students = [
+		'Status:'=>'ok',
+		'list'=>['Addam','Kat','Derp']
+		];
+	return response()->json($students);
+});
+
+// Grouping pages 
+Route::group(['prefix'=>'admin'],function(){
+	Route::get('/hello/{name?}',function($name=null){
+	return 'Hello' . $name;
+	});
+	Route::get('/students/json',function(){
+	$students = ['Addam','Kat','Derp'];
+	return response()->json($students);
+	});
+});
+
+//Routing to controller
+Route::get('/news','NewsController@index');
